b847097a960a018d641819715e5391834c115e9d
Ernesto Laval <ernesto.laval@gmail.com>
2018-01-18 19:13:33 -0300

    Add test.js simple callback

---
+++ src/test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('Loading function');
+
+/**
+ * Provide an event that contains the following keys:
+ *
+ *   - operation: one of the operations in the switch statement below
+ *   - tableName: required for operations that interact with DynamoDB
+ *   - payload: a parameter to pass to the operation being performed
+ */
+
+
+exports.handler = (event, context, callback) => {
+   callback(null,{
+    headers:{
+        "Content-Type":"application/json"
+    },
+    statusCode: 200,
+    body:JSON.stringify({msg:123})});
+};
+
+
